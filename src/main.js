056d277d522cf72c33822c3da6681122f717b058
Amaaq <maaqoul.adil@gmail.com>
2023-09-24 12:17:40 +0100

    uninstalling vuetify

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import '../assets/main.css'
-import {createVuetify} from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+
 import { createApp } from 'vue'
 import App from './App.vue'
-const vuetify = createVuetify({
-    components,
-    directives
-})
-createApp(App).use(vuetify).mount('#app')
+
+createApp(App).mount('#app')
